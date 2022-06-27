86ba299ee4bfbca5bc66c42d6fe58b4276cb2689
chouxianyu <774675548@qq.com>
2022-06-27 21:42:30 +0800

    init Builder

---
+++ Builder/v1/builder.cpp
@@ -0,0 +1,74 @@
+class House{
+    //....
+};
+
+class HouseBuilder {
+public:
+    House* pHouse;
+
+    House* GetResult(){
+        return pHouse;
+    }
+    virtual ~HouseBuilder(){}
+	virtual void BuildPart1()=0;
+    virtual void BuildPart2()=0;
+    virtual bool BuildPart3()=0;
+    virtual void BuildPart4()=0;
+    virtual void BuildPart5()=0;
+	
+};
+
+class StoneHouse: public House{
+    
+};
+
+class StoneHouseBuilder: public HouseBuilder{
+public:
+    
+    virtual void BuildPart1(){
+        //pHouse->Part1 = ...;
+    }
+    virtual void BuildPart2(){
+        
+    }
+    virtual bool BuildPart3(){
+        
+    }
+    virtual void BuildPart4(){
+        
+    }
+    virtual void BuildPart5(){
+        
+    }
+    
+};
+
+
+class HouseDirector{
+    
+public:
+    HouseBuilder* pHouseBuilder;
+    
+    HouseDirector(HouseBuilder* pHouseBuilder){
+        this->pHouseBuilder=pHouseBuilder;
+    }
+    
+    House* Construct(){
+        
+        pHouseBuilder->BuildPart1();
+        
+        for (int i = 0; i < 4; i++){
+            pHouseBuilder->BuildPart2();
+        }
+        
+        bool flag=pHouseBuilder->BuildPart3();
+        
+        if(flag){
+            pHouseBuilder->BuildPart4();
+        }
+        
+        pHouseBuilder->BuildPart5();
+        
+        return pHouseBuilder->GetResult();
+    }
+};
